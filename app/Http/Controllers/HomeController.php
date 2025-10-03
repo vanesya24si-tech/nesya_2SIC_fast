e606b1bf76c19c2481357ad21cc1d943d546b810
Nesyapcr <vanesya24si@mahasiswa.pcr.ac.id>
2025-10-03 08:17:29 +0700

    ubah 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
 {
 		/* Cara 1 */
     $data ['username']        = 'Heroku';
-    $data ['last_login']      = date('Y-m-d H:i:s');
+    $data ['login']      = date('Y-m-d H:i:s');
     $data ['pendidikan'] = ['SD','SMP','SMA','S1','S2','S3'];
     return view('home', $data );
 }
